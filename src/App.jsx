90f32d64e2560135e7c5755cd85bd54e4d4b92e0
Jeronimo Daniel Itzcovich <jditzco@hotmail.com>
2023-08-28 08:33:15 -0300

    sad

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Container, Row, Col, Form, InputGroup, Button, ButtonGroup } from 'react-bootstrap'
+import './ListadoEjercicios.css'
+import NavBar from '../components/NavBar'
+import Ejercicio from '../components/Ejercicio'
+import { useEffect, useRef, useState } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const ListadoArticulos = () => {
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    // Lista de ejercicios
+    const [listaArticulos, setListaArticulos] = useState([])
+
+    // Los ejercicios de la lista que se están mostrando en pantalla
+    const [articulosActivos, setArticulosActivos] = useState([])
+    // Filtros
+    const [dificultad, setDificultad] = useState(0)
+    const [busqueda, setBusqueda] = useState('')
+
+    const buscarInput = useRef()
+
+    useEffect(() => async() => {
+        const response = await fetch('http://localhost:5000/ejercicios')
+        const data = await response.json()
+        setListaArticulos(data)
+        setEjerciciosActivos(data)
+    }, [])
+
+    useEffect(() => { // Filtrar
+        var lista = [...listaArticulos]
+        if (dificultad) lista = lista.filter(ej => ej.dificultad === dificultad)
+        if (busqueda) {
+            lista = lista.filter((ej) => (
+                ej.titulo.toUpperCase().includes(buscarInput.current.value.toUpperCase())
+            ))
+        }
+        setArticulosActivos(lista)
+    }, [busqueda, dificultad])
+
+    const handleClick = e => setDificultad(Number(e.target.value))
+    const handleChange = e => setBusqueda(e.target.value)
+
+    return (
+        <>
+            <NavBar />
+            <Container fluid className='app-container'>
+                <Row>
+                    <Col sm={3}></Col>
+                    <Col sm={9}>
+                        <InputGroup className="mb-3">
+                            <Form.Control placeholder="Buscar..."
+                                value={busqueda}
+                                onChange={handleChange}
+                                ref={buscarInput} />
+                            <Button variant="secondary"><i className="bi bi-search"></i></Button>
+                        </InputGroup>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col sm={3} className='dificultades-container'>
+                        <h4>Dificultades</h4>
+                        <Button variant="primary" value={0} onClick={handleClick} className='dificulty-button'>Reset</Button>
+                        <ButtonGroup aria-label="Basic example" className='dificulty-button'>
+                            <Button variant="success" value={1} onClick={handleClick}>Principiante</Button>
+                            <Button variant="warning" value={2} onClick={handleClick}>Intermedio</Button>
+                            <Button variant="danger" value={3} onClick={handleClick}>Avanzado</Button>
+                        </ButtonGroup>
+                    </Col>
+                    <Col sm={9} className='ejercicios-container'>
+                        {articulosActivos.map((ej, key) => (
+                            <Ejercicio key={key} idEj={ej.id} titulo={ej.titulo} descripcion={ej.descripcion} dificultad={ej.dificultad} />
+                        ))}
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    )
 }
 
-export default App
+export default ListadoArticulos
